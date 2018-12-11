4f7de7880c8cfea688948e39fd90f29381f09cae
Carl-Gustaf Ewerbring <ewerbring@gmail.com>
2018-12-11 13:05:59 -0500

    website

---
+++ maincollection.js
@@ -0,0 +1,18 @@
+
+
+var modalBtns = [...document.querySelectorAll(".button")];
+modalBtns.forEach(function(btn){
+  btn.onclick = function() {
+    var modal = btn.getAttribute('data-modal');
+    document.getElementById(modal).style.display = "block";
+  }
+});
+
+var closeBtns = [...document.querySelectorAll(".close")];
+closeBtns.forEach(function(btn){
+  btn.onclick = function() {
+    var modal = btn.closest('.modal');
+    modal.style.display = "none";
+  }
+});
+
